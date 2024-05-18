b88239a2221e62b60bb34abe3946b02cf7a91c1f
lluisbessasanz <lluisbessasanz@gmail.com>
2024-05-18 22:01:38 +0200

    JSON amb dades

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,9 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
@@ -17,3 +20,36 @@ exports.helloWorld = onRequest((request, response) => {
    logger.info("Hello logs!", {structuredData: true});
    response.send("Hello from Firebase!");
 });
+
+initializeApp();
+
+const db = getFirestore();
+
+// ADD DATA
+
+const docRef = db.collection('users').doc('alovelace');
+
+await docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
+
+
+const aTuringRef = db.collection('users').doc('aturing');
+
+await aTuringRef.set({
+  'first': 'Alan',
+  'middle': 'Mathison',
+  'last': 'Turing',
+  'born': 1912
+});
+
+// READ DATA
+
+const snapshot = await db.collection('users').get();
+
+snapshot.forEach((doc) => {
+  console.log(doc.id, '=>', doc.data());
+});
+
